c4cc5e5a2d0c8148adabef3db4ec3b752bb05ff2
YellowFlash2012 <cyclop9@hotmail.com>
2022-10-30 07:12:55 +0100

    designed and implemented the calculation of the average cost of all courses in the course model and then updated the bootcamp model with that value

--- models/Course.js
+++ models/Course.js
@@ -37,6 +37,39 @@ const courseSchema = mongoose.Schema(
     { timestamps: true }
 );
 
+courseSchema.statics.getAverageCost=async function (bootcampID) {
+    console.log('Calculating average cost...'.blue.bold);
+
+    const obj = await this.aggregate([
+        {
+            $match: { bootcamp: bootcampID }
+        },
+        {
+            $group: {
+                _id: '$bootcamp',
+                averageCost: { $avg: '$tuition' }
+            }
+        }
+    ]);
+    console.log(obj);
+
+    try {
+        await this.model('Bootcamp').findByIdAndUpdate(bootcampID, { averageCost: Math.ceil(obj[0].averageCost / 10) * 10 });
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+// ***call getAvgCost after save
+courseSchema.post('save', function () {
+    this.constructor.getAverageCost(this.bootcamp);
+})
+
+// ***call getAvgCost before remove
+courseSchema.post('remove', function () {
+    this.constructor.getAverageCost(this.bootcamp);
+})
+
 const Course = mongoose.model("Course", courseSchema);
 
 export default Course
