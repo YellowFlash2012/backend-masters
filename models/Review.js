03886922db25b93844bba32500ca07858d6a72e0
YellowFlash2012 <cyclop9@hotmail.com>
2022-11-02 06:19:08 +0100

    modified the review model to add average rating calculation

--- models/Review.js
+++ models/Review.js
@@ -36,6 +36,42 @@ const reviewSchema = mongoose.Schema(
 // user can add only 1 review per bootcamp
 reviewSchema.index({ bootcamp: 1, user: 1 }, { unique: true });
 
+// static method to get avg rating
+reviewSchema.statics.getAverageRating = async function (bootcampID) {
+    console.log("Calculating average rating...".blue.bold);
+
+    const obj = await this.aggregate([
+        {
+            $match: { bootcamp: bootcampID },
+        },
+        {
+            $group: {
+                _id: "$bootcamp",
+                averageRating: { $avg: "$rating" },
+            },
+        },
+    ]);
+    console.log(obj);
+
+    try {
+        await this.model("Bootcamp").findByIdAndUpdate(bootcampID, {
+            averageRating: obj[0].averageRating,
+        });
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+// ***call getAvgCost after save
+reviewSchema.post("save", function () {
+    this.constructor.getAverageRating(this.bootcamp);
+});
+
+// ***call getAvgCost before remove
+reviewSchema.post("remove", function () {
+    this.constructor.getAverageRating(this.bootcamp);
+});
+
 
 const Review = mongoose.model("Review", reviewSchema);
 
